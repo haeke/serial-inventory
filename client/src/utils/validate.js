184c3bf1e6c827dc526433c69be8ce841eb5de23
haeke <riverae2@student.wpunj.edu>
2019-03-26 12:45:31 -0400

    added a helper function that will be used to ensure input fields are not left blank.

---
+++ client/src/utils/validate.js
@@ -0,0 +1,22 @@
+export const validate = values => {
+  let errors = {};
+  // This function will check to see if the values that were past are not empty. Ensure that the softwarename, software company, serial number and date aquired fields are not empty. If so, we can use the error object to indicate this to the user.
+  console.log("the values called inside vlidate ", values);
+  if (!values.softwareName) {
+    errors.softwareName = "The Software Name Field is required";
+  }
+
+  if (!values.softwareCompany) {
+    errors.softwareCompany = "The Company Name Field is required.";
+  }
+
+  if (!values.serialNumber) {
+    errors.serialNumber = "The Serial Key Field is required.";
+  }
+
+  if (!values.dateAquired) {
+    errors.dateAquired = "The Date Aquired Field is required.";
+  }
+
+  return errors;
+};
