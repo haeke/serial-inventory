ed757338480dbcc45349c5c5eadbbf15e4abf900
haeke <riverae2@student.wpunj.edu>
2019-03-26 12:38:21 -0400

    created the redux store, added the middlware that is going to be used in the project

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,25 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
-import reduxTunk from "redux-thunk";
+import reduxThunk from "redux-thunk";
+// import the root reducer
+import rootReducer from "./reducers/index";
 
 import "./index.css";
-import App from "./App";
+import App from "./components/App/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// create an object that contains all the middleware to be used
+const middleware = [logger, reduxThunk];
+
+// create the store object
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
