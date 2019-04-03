0948f985bc161bfac927e9fea1b273c3c78b7e35
haeke <riverae2@student.wpunj.edu>
2019-04-03 11:41:12 -0400

    changed the server that is used to get the json data when in production.

--- client/src/api/inventory.js
+++ client/src/api/inventory.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+let baseURL = "http://localhost:3002";
+
+if (process.env.NODE_ENV === "production") {
+  baseURL = "https://serial-inventory-json-server.herokuapp.com";
+}
+
 // We are seting the baseURL to the json-server's local host and port for now.
 export default axios.create({
-  baseURL: "http://localhost:3002"
+  baseURL: baseURL
 });
