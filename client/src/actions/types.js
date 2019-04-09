0ab5bb166a0f18acd5ed47ccba06a91c91e666fd
haeke <riverae2@student.wpunj.edu>
2019-04-09 16:21:21 -0400

    added action types for the errors reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,3 +7,7 @@ export const OPEN_MODAL = "OPEN_MODAL";
 export const CLOSE_MODAL = "CLOSE_MODAL";
 // Boolean value that will determine when to conditionally render the edit form
 export const EDIT_MODE = "EDIT_MODE";
+
+// error objects
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
