e11670650b9c96afe6ed738945ca0de166086f13
haeke <riverae2@student.wpunj.edu>
2019-04-12 13:37:51 -0400

    added a route that will return an individual software item by querying the Software database by the ID, the ID will be passed to the route as a query parameter req.params.id.

--- routes/software.js
+++ routes/software.js
@@ -13,4 +13,57 @@ router.get("/test", (req, res) => {
   res.send({ Software: "route is working." });
 });
 
+//@route GET /api/software/
+//@desc Will return a JSON object of the software saved inside the database- the softwareName, softwareCompany, serialNumber and dateAquired will be sent from the client.
+//@access Public for now but it will be Private - a user needs to be authenticated in order to create
+
+router.get("/", async (req, res) => {
+  try {
+    let software = await Software.find().sort({ date: -1 });
+
+    res.json(software);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+//@route GET /api/software/:id
+//@desc Get an individual software object by id from the Software model
+//@access Public but will be Private
+router.get("/:id", async (req, res) => {
+  try {
+    let software = await Software.findById(req.params.id);
+    res.json(software);
+  } catch (error) {
+    res.sendStatus(404).json(error);
+  }
+});
+
+//@route POST /api/software/
+//@desc Will create a document inside of the Software Model. This will consist of the softwareName, softwareCompany, serialNumber, and dateAquired.
+//@access Public but will be Private - a user will need to be authenticated.
+router.post("/", async (req, res) => {
+  console.log(req.user);
+  try {
+    const {
+      softwareName,
+      softwareCompany,
+      serialNumber,
+      dateAquired
+    } = req.body;
+    // TODO: check to make sure none of these fields are left empty.
+    let softwareEntry = await Software({
+      softwareName,
+      softwareCompany,
+      serialNumber,
+      dateAquired
+    });
+
+    softwareEntry.save();
+    res.json(softwareEntry);
+  } catch (error) {
+    res.sendStatus(404).json(error);
+  }
+});
+
 module.exports = router;
